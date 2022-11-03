81756d7ac1aab6f2e8a61e571bc1b5c947fbc316
Bashobi Roy <bashobiroy@yahoo.com>
2022-11-03 13:17:21 -0700

    Removed frontend files

--- report/executable.py
+++
@@ -1,8 +0,0 @@
-from report.survey_gen import Choices
-from report.survey_gen import HtmlSurvey
-
-htmlsurvey = HtmlSurvey()
-answer = htmlsurvey.get()
-surveyresult = Choices(answer=answer)
-print_result: str = surveyresult.get_result_text()
-print(print_result)
